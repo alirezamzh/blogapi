78775eadddd3d922151efdf4b59b24493c8ea4a0
Alireza Memarzade <alireza.am885@gmail.com>
2021-06-17 18:41:47 +0430

    update view.py and permission.py

--- posts/permissions.py
+++ posts/permissions.py
@@ -1,21 +1,14 @@
-class BasePermission(object):
-    """
-    A base class from whitch all permission classes should inherit.
-    """
+from rest_framework import permissions
 
-    def has_permission(self,request,view):
-        """
-        Return `True` if permission is granted, `False` otherwise.
-        """
-        return True
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    
+    def has_object_permission(self, request, view, obj):
+        # Raed-only permissions are allowed for any request
+        if request.method in permissions.SAFE_METHODS:
+            return True
 
-
-    def has_object_permission(self,request,view,obj):
-        """
-        Return `True` if permission is granted, `False` otherwise. 
-        """
-        return True
-
+        # Write permissions are only allowed to the author of a post
+        return obj.author == request.user
 
 
 
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics,permissions
 
 from .models import Post
+from .permissions import IsAuthorOrReadOnly
 from .serializers import PostSerializer
 
 
@@ -11,6 +12,7 @@ class PostList(generics.ListCreateAPIView):
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     # permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
